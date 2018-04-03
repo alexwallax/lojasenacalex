9cee420cc6a706639a91433f3a517876d1f73388
Weskley Bezerra <weskleymb@hotmail.com>
2018-04-02 21:14:15 -0300

    Aula de 02/04/2018

--- classes/dao/ProdutoDAO.php
+++ classes/dao/ProdutoDAO.php
@@ -3,7 +3,7 @@
 class ProdutoDAO {
 
     private $conexao;
-    
+
     public function __construct() {
         $this->conexao = Conexao::conectar();
     }
@@ -15,16 +15,16 @@ class ProdutoDAO {
                 . "{$produto->getPreco()}, {$produto->getCategoria()->getId()})";
         return mysqli_query($this->conexao, $sql);
     }
-    
+
     public function listarTodos() {
         $produtos = array();
         $sql = "select * from produtos p join categorias c on c.id=p.categoria_id";
         $resultado = mysqli_query($this->conexao, $sql);
-        while ($produto_array = mysqli_fetch_assoc($resultado)) {
+        while ($produto_array = mysqli_fetch_array($resultado)) {
             $categoria = new Categoria();
             $categoria->setId($produto_array[5]);
             $categoria->setNome($produto_array[6]);
-            
+
             $produto = new Produto();
             $produto->setId($produto_array[0]);
             $produto->setNome($produto_array[1]);
@@ -35,24 +35,38 @@ class ProdutoDAO {
         }
         return $produtos;
     }
-    
-    
-    public function remover(Categoria $categoria) {
-        $sql = "delete from categorias where id={$categoria->getId()}";
-        return mysqli_query($this->conexao, $sql);
-    }
-    
-    public function editar(Categoria $categoria) {
-        $sql = "update categorias set "
-                . "nome='{$categoria->getNome()}' "
-                . "where id={$categoria->getId()}";
+
+    public function remover(Produto $produto) {
+        $sql = "delete from produtos where id={$produto->getId()}";
         return mysqli_query($this->conexao, $sql);
     }
 
-    
-    
-    public function buscarPorId($id) {
-        
+    public function editar(Produto $produto) {
+        $sql = "update produtos set "
+                . "nome='{$produto->getNome()}', "
+                . "descricao='{$produto->getDescricao()}', "
+                . "preco={$produto->getPreco()}, "
+                . "categoria_id={$produto->getCategoria()->getId()} "
+                . "where id={$produto->getId()}";
+        return mysqli_query($this->conexao, $sql);
     }
-    
+
+    public function buscarPorId($id) {
+        $sql = "select * from produtos p join categorias c on c.id=p.categoria_id where p.id={$id}";
+        $resultado = mysqli_query($this->conexao, $sql);
+        $produto = new Produto();
+        while ($produto_array = mysqli_fetch_array($resultado)) {
+            $categoria = new Categoria();
+            $categoria->setId($produto_array[5]);
+            $categoria->setNome($produto_array[6]);
+
+            $produto->setId($produto_array[0]);
+            $produto->setNome($produto_array[1]);
+            $produto->setDescricao($produto_array[2]);
+            $produto->setPreco($produto_array[3]);
+            $produto->setCategoria($categoria);
+        }
+        return $produto;
+    }
+
 }
--- produtos.php
+++ produtos.php
@@ -5,47 +5,132 @@ require_once "./classes/dao/CategoriaDAO.php";
 require_once "./classes/modelo/Produto.php";
 require_once "./classes/dao/ProdutoDAO.php";
 
-if (isset($_POST['nome'])) {
+$produto = new Produto();
+$produtoDao = new ProdutoDAO();
+
+if (isset($_POST['salvar'])) {
     $categoria = new Categoria();
     $categoria->setId($_POST['categoria']);
-    
-    $produto = new Produto();
+
+    $produto->setId($_POST['id']);
     $produto->setNome($_POST['nome']);
     $produto->setDescricao($_POST['descricao']);
     $produto->setPreco($_POST['preco']);
     $produto->setCategoria($categoria);
+
+    if ($produto->getId() == "") {
+        $produtoDao->inserir($produto);
+    } else {
+        $produtoDao->editar($produto);
+    }
     
-    $produtoDao = new ProdutoDAO();
-    $produtoDao->inserir($produto);
+    $produto = new Produto();
 }
+
+if (isset($_POST['editar'])) {
+    $produto = $produtoDao->buscarPorId($_POST['id_produto']);
+}
+
+if (isset($_POST['remover'])) {
+    $p = new Produto();
+    $p->setId($_POST['id_produto']);
+    $produtoDao->remover($p);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Produtos</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
     </head>
     <body>
-        <form action="produtos.php" method="post">
-            <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome"><br>
-            <label for="descricao">Descrição</label>
-            <textarea id="descricao" name="descricao"></textarea><br>
-            <label for="categoria">Categoria</label>
-            <select id="categoria" name="categoria">
-                <?php
-                    $dao = new CategoriaDAO();
-                    $categorias = $dao->listarTodos();
-                    foreach ($categorias as $categoria) {
-                ?>
-                <option value="<?= $categoria->getId() ?>"><?= $categoria->getNome() ?></option>
-                <?php
-                    }
-                ?>
-            </select><br>
-            <label for="preco">Preço</label>
-            <input type="text" id="preco" name="preco"><br>
-            <button type="submit">Salvar</button>
-        </form>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <fieldset>
+                        <legend>Dados do Produto</legend>
+                        <form action="produtos.php" method="post">
+                            <input type="hidden" name="id" value="<?= $produto->getId() ?>">
+                            <div class="form-group">
+                                <label for="nome">Nome</label>
+                                <input type="text" class="form-control" id="nome" name="nome" value="<?= $produto->getNome() ?>" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <label for="descricao">Descrição</label>
+                                <textarea class="form-control" id="descricao" name="descricao"><?= $produto->getDescricao() ?></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="categoria">Categoria</label>
+                                <select class="form-control" id="categoria" name="categoria">
+                                    <?php
+                                    $dao = new CategoriaDAO();
+                                    $categorias = $dao->listarTodos();
+                                    foreach ($categorias as $categoria) {
+                                        $selecionado = "";
+                                        if ($produto->getId() != null && $categoria->getId() == $produto->getCategoria()->getId()) {
+                                            $selecionado = "selected";
+                                        }
+                                        ?>
+                                        <option value="<?= $categoria->getId() ?>" <?= $selecionado ?>><?= $categoria->getNome() ?></option>
+                                        <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="preco">Preço</label>
+                                <input type="text" class="form-control" id="preco" name="preco" value="<?= $produto->getPreco() ?>">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block" name="salvar">Salvar</button>
+                        </form>
+                    </fieldset>
+                </div>
+                <div class="col-sm-6">
+                    <fieldset>
+                        <legend>Lista de Produtos</legend>
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Produto</th>
+                                    <th>Categoria</th>
+                                    <th>Preço</th>
+                                    <th colspan="2">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $produtos = $produtoDao->listarTodos();
+                                foreach ($produtos as $p) {
+                                    ?>
+                                    <tr>
+                                        <td><?= $p->getId() ?></td>
+                                        <td><?= $p->getNome() ?></td>
+                                        <td><?= $p->getCategoria()->getNome() ?></td>
+                                        <td><?= $p->getPreco() ?></td>
+                                        <td>
+                                            <form action="produtos.php" method="post">
+                                                <input type="hidden" name="id_produto" value="<?= $p->getId() ?>">
+                                                <button type="submit" class="btn btn-primary" name="editar">editar</button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <form action="produtos.php" method="post">
+                                                <input type="hidden" name="id_produto" value="<?= $p->getId() ?>">
+                                                <button type="submit" class="btn btn-danger" name="remover">remover</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
